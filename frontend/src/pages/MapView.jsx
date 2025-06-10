52b79c7a77d515c0cc979392cea4dfbfc7b628db
lucas <lucassanner04@gmail.com>
2025-06-10 22:32:36 +0200

    Testversion von der Map_View

---
+++ frontend/src/pages/MapView.jsx
@@ -0,0 +1,14 @@
+
+import { Application } from "@pixi/react";
+import MapStage from "../components/MapStage.jsx";
+
+const Game = () => {
+    return (
+        <Application autoStart sharedTicket />
+    );
+}
+
+export default Game;
+
+
+
