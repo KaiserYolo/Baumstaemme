26bd46b1c3b769fef62dbde7e0ba990b7016c3ce
dominik <seifert.dominik01@gmail.com>
2025-06-17 17:09:18 +0200

    Login Page Login Form Login Function

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,60 @@
+import {StrictMode, useState} from "react";
+import {loginUser} from "../services/AuthAPI.js";
+import '../App.css';
+import WoodBox from "./WoodBox.jsx";
+
+export default function Form(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const buttonId = e.nativeEvent.submitter.id;
+
+        if(buttonId === "login"){
+           console.log("login attempt");
+            const user = await loginUser({username, password});
+        }
+        else if(buttonId === "register"){
+            console.log("register attempt");
+            //const user = await registerUser({username, password});
+        }
+        else{
+            console.log("error during login or register");
+        }
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="login-form">
+            <div className="form-box">
+                <h1 className="form-top">Login</h1>
+                <div className="form-middle">
+                    <a className="form-error">{usernameError}</a>
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input id="username" onChange={e => setUsername(e.target.value)} value={username} type="text" placeholder="Username" required={true} />
+                    </div>
+
+                    <a className="form-error">{passwordError}</a>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder="Password" required={true} />
+                    </div>
+                </div>
+                <div className="form-bottom">
+                    <button type="submit" id="login" className="login-button" >
+                        <WoodBox n={5} text="Login" className="header-title" />
+                    </button>
+                    <button type="submit" id="register" className="login-button" >
+                        <WoodBox n={5} text="Register" className="header-title" />
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
+
+}
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
+import Form from '../components/Form.jsx';
+import '../App.css';
+
 export default function LoginPage() {
     return (
         <main className="main">
             <div className="main-content" >
-                <label>Login Page</label>
+                <Form />
             </div>
         </main>
     );
