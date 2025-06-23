88d9bfa5c5683c58bfb5ee1e72270fd8795a87cf
lucas <lucassanner04@gmail.com>
2025-06-23 10:15:16 +0200

    Da war ein div zu viel :)

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -3,11 +3,11 @@ import '../Map.css'
 
 export default function GamePage() {
     return (
-        <div>
             <div className="main-content" >
               <div className='Map'>
                 <MapView/>
             </div>
         </div>
+
     );
 }
