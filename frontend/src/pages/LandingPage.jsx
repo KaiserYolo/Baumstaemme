db5b5bf1a8858e7ab433aaf24529736386c1011d
dominik <seifert.dominik01@gmail.com>
2025-06-30 23:02:28 +0200

    Landing Page hübsch gemacht :)

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,10 +2,36 @@ export default function LandingPage() {
     return (
         <main className="main">
             <div className="main-content" >
-                <label>Landing Page</label>
+                <label className="intro-text">
+                    Welcome Warrior,<br/>
+                    to the amazing world of Die Baumstämme,<br/>
+                    where you either dominate your enemies,<br/>
+                    or fall victim to their wrath!<br/><br/>
+                    Sign up today!
+                </label>
             </div>
             <div className="main-content">
-                <label>Recent Updates</label>
+                <div className="update-table">
+                    <h1>
+                        - Recent Updates -
+                    </h1>
+                    <div className="update-element">
+                        <label className="update-element-title">
+                            Game Release
+                        </label>
+                        <label className="update-element-subtitle">
+                            Postponed
+                        </label>
+                    </div>
+                    <div className="update-element">
+                        <label className="update-element-title">
+                            Start of Game Development
+                        </label>
+                        <label className="update-element-subtitle">
+                            May 2025
+                        </label>
+                    </div>
+                </div>
             </div>
         </main>
     );
