6e3dba85b76bcebb876cd0e2cfa5943983bba738
lucas <lucassanner04@gmail.com>
2025-06-30 10:11:06 +0200

    CreateGameAPI.js existiert mit Logik

--- frontend/src/services/CreateGameAPI.js
+++ frontend/src/services/CreateGameAPI.js
@@ -1,3 +1,21 @@
-export const createGameAPI = async (user) => {
+export const createGameAPI = async (name, size) => {
+    try{
+        const url = new URL (`http://localhost:8080/api/games`);
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(name, size),
+            credentials: "include",
+        })
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const json = await response.json()
+        console.log(json);
+        return json;
+
+    } catch(error) {
+        console.log(error);
+    }
 
 }
